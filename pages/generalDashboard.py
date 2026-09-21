6cd3fb4afa1c4d4f5ce94d051771d8fc1151d738
YungKamLun6000 <106392950+YungKamLun6000@users.noreply.github.com>
2026-09-21 09:58:08 +0800

    Built muti Filter for month and year

--- pages/generalDashboard.py
+++ pages/generalDashboard.py
@@ -18,8 +18,21 @@ LibraryVisitsbyExternalVisitors = ["Total No. of Visits", "Total No. of Visitors
 dash.register_page(__name__)
 
 #Components
-month_slider = dcc.Slider(1, 12, 1,value=6,id='month')
-year_slider = dcc.Slider(2016, 2025, 1,value=2025, id='year')
+month_slider = dcc.Dropdown(
+    [i for i in range(1, 13)] ,
+    [6],
+    debounce=True,
+    multi=True,
+    id='month'
+)
+
+year_slider = dcc.Dropdown(
+    [i for i in range(2016, 2027)] ,
+    [2025],
+    debounce=True,
+    multi=True,
+    id ='year'
+)
 
 cards=[
     dbc.Col(
@@ -31,7 +44,7 @@ layout = dbc.Container([
     html.H1("General Dashboard", className="text-center my-4"),
     year_slider,
     month_slider,
-    dcc.RadioItems(options=['Summary Page - Facts', 'Summary Page for Section Heads', 'General Summary','Library Instruction Service'], value='Summary', id='controls-and-radio-item'),
+    dcc.RadioItems(options=['Summary Page - Facts', 'Summary Page for Section Heads', 'General Summary','Library Instruction Service'], value='General Summary', id='controls-and-radio-item'),
     html.Div(id='output_summary'),
 ], fluid=True)
 
@@ -45,35 +58,43 @@ layout = dbc.Container([
 )
 
 def update_cards(selected_value, month, year):
+    if not month or not year:
+        return html.Div("Please select at least one Year and Month.", className="text-center my-4 text-danger")
+
     if selected_value == 'Summary Page - Facts':
-        Summary_Page_Facts_df_filtered_by_year = Summary_Page_Facts_df[Summary_Page_Facts_df["Year"].isin([year])]
-        Summary_Page_Facts_df_filtered_by_Month = Summary_Page_Facts_df_filtered_by_year[Summary_Page_Facts_df_filtered_by_year["Month"].isin([month])]
+        Summary_Page_Facts_df_filtered_by_year = Summary_Page_Facts_df[Summary_Page_Facts_df["Year"].isin(year)]
+        Summary_Page_Facts_df_filtered_by_Month = Summary_Page_Facts_df_filtered_by_year[Summary_Page_Facts_df_filtered_by_year["Month"].isin(month)]
+        Summary_Page_Facts_df_filtered_by_Month = Summary_Page_Facts_df_filtered_by_Month.groupby('DisplayName', as_index=False).agg('sum')
         Summary_Page_Facts = Summary_Page_Facts_df_filtered_by_Month.to_dict(orient="records")
         filtered_data = Summary_Page_Facts
 
         gridNumSetting = 4
         cards = Setting_Layout.updated_card(filtered_data, gridNumSetting)
     elif selected_value == 'Summary Page for Section Heads':
-        Summary_Page_Facts_df_filtered_by_year = Summary_Page_Facts_df[Summary_Page_Facts_df["Year"].isin([year])]
-        Summary_Page_Facts_df_filtered_by_Month = Summary_Page_Facts_df_filtered_by_year[Summary_Page_Facts_df_filtered_by_year["Month"].isin([month])]
+        Summary_Page_Facts_df_filtered_by_year = Summary_Page_Facts_df[Summary_Page_Facts_df["Year"].isin(year)]
+        Summary_Page_Facts_df_filtered_by_Month = Summary_Page_Facts_df_filtered_by_year[Summary_Page_Facts_df_filtered_by_year["Month"].isin(month)]
+        Summary_Page_Facts_df_filtered_by_Month = Summary_Page_Facts_df_filtered_by_Month.groupby('DisplayName', as_index=False).agg('sum')
         Summary_General = Summary_Page_Facts_df_filtered_by_Month.to_dict(orient="records")
         filtered_data = Summary_General
 
         gridNumSetting = 3
         cards = Setting_Layout.updated_card(filtered_data, gridNumSetting)
     elif selected_value == 'General Summary':
-        General_Summary_df_filtered_by_year = General_Summary_df[General_Summary_df["Year"].isin([year])]
-        General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_year[General_Summary_df_filtered_by_year["Month"].isin([month])]
+        General_Summary_df_filtered_by_year = General_Summary_df[General_Summary_df["Year"].isin(year)]
+        General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_year[General_Summary_df_filtered_by_year["Month"].isin(month)]
         General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_Month[~General_Summary_df_filtered_by_Month["DisplayName"].isin(LibraryInstructionService)]
+        General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_Month.groupby('DisplayName', as_index=False).agg('sum')
         General_Summary = General_Summary_df_filtered_by_Month.to_dict(orient="records")
         filtered_data = General_Summary
 
         gridNumSetting = 4
         cards = Setting_Layout.updated_card(filtered_data, gridNumSetting)
     else:
-        General_Summary_df_filtered_by_year = General_Summary_df[General_Summary_df["Year"].isin([year])]
-        General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_year[General_Summary_df_filtered_by_year["Month"].isin([month])]
+        General_Summary_df_filtered_by_year = General_Summary_df[General_Summary_df["Year"].isin(year)]
+        General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_year[General_Summary_df_filtered_by_year["Month"].isin(month)]
         General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_Month[General_Summary_df_filtered_by_Month["DisplayName"].isin(LibraryInstructionService)]
+
+        General_Summary_df_filtered_by_Month = General_Summary_df_filtered_by_Month.groupby('DisplayName', as_index=False).agg('sum')
 
         Vistors_df = General_Summary_df_filtered_by_Month[General_Summary_df_filtered_by_Month["DisplayName"].isin(LibraryVisitsbyExternalVisitors)]
         Library_df = General_Summary_df_filtered_by_Month[~General_Summary_df_filtered_by_Month["DisplayName"].isin(LibraryVisitsbyExternalVisitors)]
